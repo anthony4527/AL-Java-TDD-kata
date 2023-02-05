95d88344a8c508c14ef7f6ae89feedb1df0d1bd5
AnthonyLee <anthony@calglobal.com>
2023-02-05 23:26:00 +0800

    convert 1 to roman numeral

---
+++ src/main/java/com/techreturn/RomanNumeralsConverter.java
@@ -0,0 +1,7 @@
+package com.techreturn;
+
+public class RomanNumeralsConverter {
+    public String convert(int arabicNumber) {
+        return "I";
+    }
+}
---
+++ src/test/java/com/techreturn/RomanNumeralsConverterTest.java
@@ -0,0 +1,16 @@
+package com.techreturn;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumeralsConverterTest {
+    @Test
+    public void checkRomanNumeralforOne() {
+        RomanNumeralsConverter romanNumeralsConverter = new RomanNumeralsConverter();
+        assertEquals("I", romanNumeralsConverter.convert(1));
+    }
+
+}
