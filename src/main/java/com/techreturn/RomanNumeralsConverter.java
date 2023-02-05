f41c9804375684b66ee92b3183582d259accd84a
AnthonyLee <anthony@calglobal.com>
2023-02-05 23:38:10 +0800

    convert 2 to numeral

--- src/main/java/com/techreturn/RomanNumeralsConverter.java
+++ src/main/java/com/techreturn/RomanNumeralsConverter.java
@@ -2,6 +2,10 @@ package com.techreturn;
 
 public class RomanNumeralsConverter {
     public String convert(int arabicNumber) {
-        return "I";
+        String numeral = "";
+        for (int i=0; i< arabicNumber; i++){
+            numeral += "I";
+        }
+        return numeral;
     }
 }
